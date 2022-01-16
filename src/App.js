7a59545ab8ae09dd5a533d0cc01134fc36c8c6cd
ishant9804 <ishantkumar761@gmail.com>
2022-01-16 18:29:14 +0530

    added firestore to save todos in cloud and sort by time option

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { TextField, Button } from '@mui/material';
 import Todo from './components/Todo';
+import { db } from './firebase.js';
+import { collection, query, orderBy, onSnapshot, serverTimestamp, addDoc } from 'firebase/firestore';
 import './App.css';
 
+const q = query(collection(db, 'todos'), orderBy('timestamp', 'desc'))
+
 function App () {
-  const [todos, setTodos] = useState([
-    'Create a Todo app',
-    'Do DPP of Maths.'
-  ]);
+  const [todos, setTodos] = useState([]);
   const [input, setInput] = useState('');
+  useEffect(() => {
+    onSnapshot(q, (snapshot) => {
+      setTodos(snapshot.docs.map(doc => ({
+        id: doc.id,
+        item: doc.data(),
+      })))
+    })
+  }, [input]);
   const addTodo = (e) => {
     e.preventDefault();
-    setTodos([...todos, input]);
+    addDoc(collection(db, 'todos'), {
+      todo: input,
+      timestamp: serverTimestamp()
+    })
     setInput('');
   };
   return (
@@ -22,7 +34,7 @@ function App () {
         <Button variant='contained' color="primary" onClick={addTodo}>Add Todo</Button>
       </form>
       <ul>
-        {todos.map(todo => <Todo todo={todo} />)}
+        {todos.map(item => <Todo key={item.id} arr={item} />)}
       </ul>
     </div>
   );
