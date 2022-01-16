d2030f1d0c088e0d1fecb08c47ee8bd039080447
ishant9804 <ishantkumar761@gmail.com>
2022-01-16 13:13:44 +0530

    added firebase config file

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBcY0ElaTMnlTXAehZG36evsxH68Q--8c4",
+    authDomain: "workodo-bffec.firebaseapp.com",
+    projectId: "workodo-bffec",
+    storageBucket: "workodo-bffec.appspot.com",
+    messagingSenderId: "471672313589",
+    appId: "1:471672313589:web:9fdc1f75cde5244c2657bc"
+};
