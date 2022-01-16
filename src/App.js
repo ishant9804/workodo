89dd63b918f889e245d2c30048ac092f9413b14e
ishant9804 <ishantkumar761@gmail.com>
2022-01-16 13:47:13 +0530

    added the material-ui and form for todo app and some functions

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import React, { useState } from 'react';
+import { TextField, Button } from '@mui/material';
 import './App.css';
 
 function App () {
+  const [todos, setTodos] = useState([
+    'Create a Todo app',
+    'Do DPP of Maths.'
+  ]);
+  const [input, setInput] = useState('');
+  const addTodo = (e) => {
+    e.preventDefault();
+    setTodos([...todos, input]);
+    setInput('');
+  };
   return (
     <div className="App">
       <h2>Workodo - A TODO App</h2>
+      <form>
+        <TextField id="outlined-basic" label="Create Todo" variant="outlined" style={{ margin: "0px 5px" }} size='small' value={input} onChange={e => setInput(e.target.value)} />
+        <Button variant='contained' color="primary" onClick={addTodo}>Add Todo</Button>
+      </form>
+      <ul>
+        {todos.map(todo => <li>{todo}</li>)}
+      </ul>
     </div>
   );
 }
