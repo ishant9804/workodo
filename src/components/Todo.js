f1771dc13d0205371b29cbe8e107ccefda772e66
ishant9804 <ishantkumar761@gmail.com>
2022-01-16 13:55:00 +0530

    added todo.js for single todo format

---
+++ src/components/Todo.js
@@ -0,0 +1,14 @@
+import { List, ListItem, ListItemAvatar, ListItemText } from '@mui/material';
+
+const Todo = ({ todo }) => {
+    return (
+        <List className='todo_list'>
+            <ListItem>
+                <ListItemAvatar />
+                <ListItemText primary={todo} secondary={todo} />
+            </ListItem>
+        </List>
+    )
+}
+
+export default Todo;
